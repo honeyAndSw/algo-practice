3861e198dc82565c33c7d0c7ccbc5873827a3644
naheon.kim <naheon.kim@navercorp.com>
2017-11-20 10:14:40 +0900

    WIP : Kakuro2 - implement all needed

--- src/main/scala/algostrategiesscala/combisearch11/Kakuro2.scala
+++ src/main/scala/algostrategiesscala/combisearch11/Kakuro2.scala
@@ -17,22 +17,42 @@ object Kakuro2 {
   val VERTICAL = 1
 
   var BOARD: Array[Array[Int]] = _
+  var BOARD_ANSWER: Array[Array[Int]] = _
   var BOARD_HHINT: Array[Array[Int]] = _
   var BOARD_VHINT: Array[Array[Int]] = _
 
   val HHINT: mutable.Map[String, Hint] = mutable.Map.empty
   val VHINT: mutable.Map[String, Hint] = mutable.Map.empty
 
+  def solve(board: Array[Array[Int]], hints: Array[Array[Int]]): Array[Array[Int]] = {
+    initialize(board, hints)
+    val nextYX: (Int, Int) = findNextYX(0, 0).get
+    val result = fillBoard(nextYX._1, nextYX._2)
+    BOARD_ANSWER
+  }
+
+  def print: Unit = BOARD_ANSWER.foreach(row => println(row.mkString(",")))
+
   def initialize(board: Array[Array[Int]], hints: Array[Array[Int]]): Unit = {
     val len = board.length
     BOARD = board
+    BOARD_ANSWER = Array.fill(len)(Array.fill(len)(0))
+
+    // Set whitecells to -1
+    BOARD.indices.foreach { y =>
+      BOARD.indices.foreach { x =>
+        if (BOARD(y)(x) == 1) BOARD_ANSWER(y)(x) = -1
+      }
+    }
+
     BOARD_HHINT = Array.fill(len)(Array.fill(len)(0))
     BOARD_VHINT = Array.fill(len)(Array.fill(len)(0))
 
     // each described with four integers: y, x, direction, and sum
+    // y and x are 1-based integers
     hints.foreach { hint =>
-      val y = hint(0)
-      val x = hint(1)
+      val y = hint(0) - 1
+      val x = hint(1) - 1
       val direction = hint(2)
       val sum = hint(3)
 
@@ -42,26 +62,20 @@ object Kakuro2 {
       direction match {
         case HORIZONTAL =>
           BOARD_HHINT(y)(x) = sum
-          for (x1 <- 1 to numofcells) {
-            HHINT.put(yxKey(y, x1 + x), hintObj)
-          }
+          (1 to numofcells).foreach(x1 => HHINT.put(yxKey(y, x1 + x), hintObj))
         case VERTICAL =>
           BOARD_VHINT(y)(x) = sum
-          for (y1 <- 1 to numofcells) {
-            VHINT.put(yxKey(y1 + y, x), hintObj)
-          }
+          (1 to numofcells).foreach(y1 => VHINT.put(yxKey(y1 + y, x), hintObj))
       }
     }
   }
 
-  private def numberOfCells(y: Int, x: Int, direction: Int): Int = {
+  private def numberOfCells(hintY: Int, hintX: Int, direction: Int): Int = {
+    val longestOnePrefixLen: (Iterable[Int] => Int) = itr => itr.toSeq.takeWhile(_ == 1).length
+
     direction match {
-      case HORIZONTAL =>
-        BOARD(y).drop(x + 1).zipWithIndex.find(_._1 == 0).get._2
-      case VERTICAL =>
-        BOARD.indices
-          .map(yi => BOARD(yi)(x))
-          .drop(y + 1).zipWithIndex.find(_._1 == 0).get._2
+      case HORIZONTAL => longestOnePrefixLen(BOARD(hintY).drop(hintX + 1))
+      case VERTICAL => longestOnePrefixLen(BOARD.indices.map(yi => BOARD(yi)(hintX)).drop(hintY + 1))
     }
   }
 
@@ -70,34 +84,41 @@ object Kakuro2 {
   def fillBoard(y: Int, x: Int): Boolean = {
     val nextYX: Option[(Int, Int)] = findNextYX(y, x)
 
+    println(s">>>>> ($x,$y)")
     // true if nextYX is empty or map function return true
     nextYX.forall { case (nextY, nextX) =>
-      val num: Option[Int] = (1 to 9).filter { n =>
-        smallerThanHints(y, x, n) && !duplicated(y, x, n) && bigEnough(y, x, n)
-      }.map { n =>
-        BOARD(y)(x) = n
-        (n, fillBoard(nextY, nextX))
-      }
-      .find(_._2) // filter and get valid `n`
-      .map(_._1)
+      val num: Option[Int] = (1 to 9).toSeq
+        .filter { n =>
+          val predicate1 = smallerThanHints(y, x, n)
+          val predicate2 = !duplicated(y, x, n)
+          val predicate3 = bigEnough(y, x, n)
+
+          println(s">> $n $predicate1 $predicate2 $predicate3")
+          predicate1 && predicate2 && predicate3
+        }.map { n =>
+          BOARD_ANSWER(y)(x) = n
+          (n, fillBoard(nextY, nextX))
+        }
+        .find(_._2) // filter and get valid `n`
+        .map(_._1)
 
       // Reset board of set to valid `n`
-      BOARD(y)(x) = num.getOrElse(1)
+      BOARD_ANSWER(y)(x) = num.getOrElse(-1)
       num.isDefined
     }
   }
 
   private def findNextYX(y: Int, x: Int): Option[(Int, Int)] = {
-    BOARD.zipWithIndex
+    BOARD_ANSWER.zipWithIndex
       .drop(y)
       .map { case (row, yi) =>
         row.zipWithIndex
           .drop(if (yi == y) x + 1 else 0)
-          .find(_._1 == 1)
+          .find(_._1 == -1)
           .map { case (v, xi) => (yi, xi, v) }
-          .getOrElse((-1, -1, -1))
+          .getOrElse((-1, -1, -2))
       }
-      .find(_._3 > -1)
+      .find(_._3 > -2)
       .map { case (yi, xi, _) => (yi, xi) }
   }
 
@@ -106,18 +127,37 @@ object Kakuro2 {
 
   private def duplicated(y: Int, x: Int, n: Int): Boolean = {
     val hhint = HHINT(yxKey(y, x))
-    val hdup = BOARD(y).drop(hhint.x + 1).take(hhint.numofcells).contains(n)
+    val hdup = BOARD_ANSWER(y).toSeq.
+      slice(hhint.x + 1, hhint.numofcells + hhint.x + 1)
+      .contains(n)
 
     val vhint = VHINT(yxKey(y, x))
-    val vdup = BOARD.indices
-      .map(yi => BOARD(yi)(x))
+    val vdup = BOARD_ANSWER.indices
+      .map(yi => BOARD_ANSWER(yi)(x))
       .slice(vhint.y + 1, vhint.y + 1 + vhint.numofcells)
       .contains(n)
 
     hdup || vdup
   }
 
-  private def bigEnough(y: Int, x: Int, n: Int): Boolean = ???
+  private def bigEnough(y: Int, x: Int, n: Int): Boolean = {
+    val countEmptyCells: ((Iterable[Int]) => Int) = (itr) => itr.count(_ == -1)
+    val remainingSum: ((Hint, Iterable[Int]) => Int) = (hint, itr) => hint.value - itr.filter(_ > -1).sum
+
+    val hhint = HHINT(yxKey(y, x))
+    // slice from the right next of hint
+    val hhintCells = BOARD_ANSWER(y).toSeq.slice(hhint.x + 1, hhint.numofcells + hhint.x + 1)
+    val hEmptyCells = countEmptyCells(hhintCells)
+
+    val vhint = VHINT(yxKey(y, x))
+    val vhintCells = BOARD_ANSWER.indices.map(yi => BOARD_ANSWER(yi)(x)).slice(vhint.y + 1, vhint.y + 1 + vhint.numofcells)
+    val vEmptyCells = countEmptyCells(vhintCells)
+
+    // hint.value <= n + (# of empty cells * 9)
+    // hint.value - n <= # of empty cells * 9
+    ((remainingSum(hhint, hhintCells) - n) <= (hEmptyCells - 1) * 9) &&
+      ((remainingSum(vhint, vhintCells) - n) <= (vEmptyCells - 1) * 9)
+  }
 }
 
 
---
+++ src/test/scala/algostrategiesscala/combisearch11/Kakuro2Suites.scala
@@ -0,0 +1,90 @@
+package algostrategiesscala.combisearch11
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers._
+
+/**
+  * Kakuro2Suites
+  *
+  * @author naheon
+  * @since 2017. 11. 20.
+  */
+class Kakuro2Suites extends FunSuite {
+
+  val BOARD = Array(
+    Array(0, 0, 0, 0, 0, 0, 0, 0),
+    Array(0, 1, 1, 0, 0, 1, 1, 1),
+    Array(0, 1, 1, 0, 1, 1, 1, 1),
+    Array(0, 1, 1, 1, 1, 1, 0, 0),
+    Array(0, 0, 1, 1, 0, 1, 1, 0),
+    Array(0, 0, 0, 1, 1, 1, 1, 1),
+    Array(0, 1, 1, 1, 1, 0, 1, 1),
+    Array(0, 1, 1, 1, 0, 0, 1, 1)
+  )
+
+  val HINTS = Array(
+    Array(2, 1, 0, 16), // horizontal sum
+    Array(2, 5, 0, 24),
+    Array(3, 1, 0, 17),
+    Array(3, 4, 0, 29),
+    Array(4, 1, 0, 35),
+    Array(5, 2, 0, 7),
+    Array(5, 5, 0, 8),
+    Array(6, 3, 0, 16),
+    Array(7, 1, 0, 21),
+    Array(7, 6, 0, 5),
+    Array(8, 1, 0, 6),
+    Array(8, 6, 0, 3),
+    Array(1, 2, 1, 23), // vertical sum
+    Array(1, 3, 1, 30),
+    Array(1, 6, 1, 27),
+    Array(1, 7, 1, 12),
+    Array(1, 8, 1, 16),
+    Array(2, 5, 1, 17),
+    Array(3, 4, 1, 15),
+    Array(4, 7, 1, 12),
+    Array(5, 5, 1, 7),
+    Array(5, 8, 1, 7),
+    Array(6, 2, 1, 11),
+    Array(6, 3, 1, 10)
+  )
+
+  test("initialize") {
+    Kakuro2.initialize(BOARD, HINTS)
+
+    val boardHhint = Kakuro2.BOARD_HHINT.clone()
+    val boardVhint = Kakuro2.BOARD_VHINT.clone()
+
+    HINTS.foreach { arr =>
+      arr(2) match {
+        case Kakuro2.HORIZONTAL => boardHhint(arr(0) - 1)(arr(1) - 1) should equal (arr(3))
+        case Kakuro2.VERTICAL => boardVhint(arr(0) - 1)(arr(1) - 1) should equal (arr(3))
+      }
+    }
+  }
+
+  test("solve") {
+    val expected = Array(
+      Array(0, 0, 0, 0, 0, 0, 0, 0),
+      Array(0, 9, 7, 0, 0, 8, 7, 9),
+      Array(0, 8, 9, 0, 8, 9, 5, 7),
+      Array(0, 6, 8, 5, 9, 7, 0, 0),
+      Array(0, 0, 6, 1, 0, 2, 6, 0),
+      Array(0, 0, 0, 4, 6, 1, 3, 2),
+      Array(0, 8, 9, 3, 1, 0, 1, 4),
+      Array(0, 3, 1, 2, 0, 0, 2, 1)
+    )
+
+    val answer = Kakuro2.solve(BOARD, HINTS)
+    Kakuro2.print
+//    0,0,0,0,0,0,0,0
+//    0,7,-1,0,0,7,-1,-1
+//    0,9,8,0,8,-1,5,7
+//    0,8,7,5,-1,6,0,0
+//    0,0,6,1,0,8,3,0
+//    0,0,0,2,1,9,8,3
+//    0,9,8,3,6,0,1,5
+//    0,6,5,4,0,0,2,-1
+    answer should equal (expected)
+  }
+}
