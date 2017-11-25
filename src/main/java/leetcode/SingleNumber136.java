f6c77d2e98a52d0114262609469c1ea8569412a9
honey.and.sw <honey.and.sw@gmail.com>
2017-11-25 12:49:34 +0900

    leetcode 136. Single Number - another solution

--- src/main/java/leetcode/SingleNumber136.java
+++ src/main/java/leetcode/SingleNumber136.java
@@ -1,9 +1,28 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 /**
  * Created by honey.and.sw on 2017. 11. 23.
  */
 public class SingleNumber136 {
+
+    /**
+     * More simple solution than #1 using cache
+     */
+    public int singleNumber2(int[] nums) {
+        LinkedList<Integer> cache = new LinkedList<>();
+        for (int n : nums) {
+            // If use `int n` directly,
+            // LinkedList#remove(int n) will be called where n is index.
+            // LinkedList#remove(Object o) is what to delete `o`.
+            Integer in = n;
+            if (cache.contains(in)) cache.remove(in);
+            else cache.add(in);
+        }
+        return cache.get(0);
+    }
+
     public int singleNumber(int[] nums) {
         int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
         for (int n : nums) {
--- src/test/java/leetcode/SingleNumber136Test.java
+++ src/test/java/leetcode/SingleNumber136Test.java
@@ -17,8 +17,16 @@ public class SingleNumber136Test {
     }
 
     @Test
-    public void singleNumber2() throws Exception {
+    public void singleNumber_2() throws Exception {
         assertEquals(1, solution.singleNumber(new int[]{1}));
         assertEquals(-1, solution.singleNumber(new int[]{-1}));
+    }
+
+    @Test
+    public void singleNumber2() throws Exception {
+        assertEquals(5, solution.singleNumber2(new int[]{-5, -5, 5}));
+        assertEquals(3, solution.singleNumber2(new int[]{3, 4, 5, 4, 5}));
+        assertEquals(1, solution.singleNumber2(new int[]{1}));
+        assertEquals(-1, solution.singleNumber2(new int[]{-1}));
     }
 }
