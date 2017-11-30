70a18f5058aab82ab4f804f1355ac2173f694f41
honey.and.sw <honey.and.sw@gmail.com>
2017-12-01 00:44:03 +0900

    Hints

--- src/main/java/leetcode/ClimbingStairs70.java
+++ src/main/java/leetcode/ClimbingStairs70.java
@@ -10,10 +10,11 @@ public class ClimbingStairs70 {
     Map<Integer, Integer> cache = new HashMap<>();
 
     public int climbStairs(int n) {
-        if (n <= 0) return 0;
-        else if (n <= 2) return n;
+        if (n <= 2) return n;
         else if (cache.containsKey(n)) return cache.get(n);
         else {
+            // It's similar to fibonacci number,
+            // then we could make the question simple to fibonacci number calculation.
             int result = climbStairs(n - 1) + climbStairs(n - 2);
             cache.put(n, result);
             return result;
