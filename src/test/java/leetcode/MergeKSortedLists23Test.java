acb035715a062dc2252b738bd916c3362c32fae0
honey.and.sw <honey.and.sw@gmail.com>
2018-01-27 20:15:13 +0900

    ListNode toArray

---
+++ src/test/java/leetcode/MergeKSortedLists23Test.java
@@ -0,0 +1,68 @@
+package leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * MergeKSortedLists23Test
+ *
+ * @author naheon
+ * @since 2018. 01. 24.
+ */
+public class MergeKSortedLists23Test {
+	MergeKSortedLists23 solution = new MergeKSortedLists23();
+
+	@Test
+	public void test0() throws Exception {
+		ListNode[] lists = new ListNode[]{
+			new ListNode(1)
+		};
+
+		int[] merged = solution.mergeKLists(lists).toArray();
+		assertEquals(1, merged.length);
+		assertArrayEquals(new int[]{1}, merged);
+	}
+
+	@Test
+	public void test1() throws Exception {
+		ListNode[] lists = new ListNode[]{
+			new ListNode(2),
+			new ListNode(1)
+		};
+
+		int[] merged = solution.mergeKLists(lists).toArray();
+		assertEquals(2, merged.length);
+		assertArrayEquals(new int[]{1,2}, merged);
+	}
+
+	@Test
+	public void test2() throws Exception {
+		ListNode[] lists = new ListNode[]{
+			new ListNode(2, new ListNode(3)),
+			new ListNode(1, new ListNode(2))
+		};
+
+		int[] merged = solution.mergeKLists(lists).toArray();
+		assertArrayEquals(new int[]{1,2,2,3}, merged);
+	}
+
+	@Test
+	public void test3() throws Exception {
+		ListNode[] lists = new ListNode[]{
+			new ListNode(2, new ListNode(3)),
+			new ListNode(1, new ListNode(2, new ListNode(4)))
+		};
+
+		int[] merged = solution.mergeKLists(lists).toArray();
+		assertArrayEquals(new int[]{1,2,2,3,4}, merged);
+	}
+
+	@Test
+	public void test4() throws Exception {
+		assertNull(solution.mergeKLists(new ListNode[0]));
+		assertNull(solution.mergeKLists(new ListNode[]{null}));
+	}
+}
