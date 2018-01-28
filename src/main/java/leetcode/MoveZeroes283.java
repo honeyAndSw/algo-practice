1b426653dced01bfc1942309137c196381527853
honey.and.sw <honey.and.sw@gmail.com>
2018-01-28 17:57:28 +0900

    leetcode 283. Move Zeroes - Optimal solution

--- src/main/java/leetcode/MoveZeroes283.java
+++ src/main/java/leetcode/MoveZeroes283.java
@@ -5,6 +5,18 @@ package leetcode;
  */
 public class MoveZeroes283 {
 	public void moveZeroes(int[] nums) {
+		for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.length; cur++) {
+			if (nums[cur] != 0) {
+				// swap(nums[lastNonZeroFoundAt++], nums[cur]);
+				int temp = nums[cur];
+				nums[cur] = nums[lastNonZeroFoundAt];
+				nums[lastNonZeroFoundAt++] = temp;
+			}
+		}
+	}
+
+	/* Not Optimal */
+	public void moveZeroes0(int[] nums) {
 		// find the first non-zero index from the end.
 		int zeroNesPos = nums.length - 1;
 		while (zeroNesPos >= 0 && nums[zeroNesPos] == 0) zeroNesPos--;
@@ -14,11 +26,13 @@ public class MoveZeroes283 {
 		while (zero >= 0 && nums[zero] != 0) zero--;
 
 		while (zero >= 0) {
-			if (zero < zeroNesPos) {
+			// Because `zero` starts from zeroNewPos,
+			// (zero < zeroNewPos) is always true.
+			// if (zero < zeroNesPos) {
 				for (int i = zero; i < zeroNesPos; i++) nums[i] = nums[i + 1];
 				nums[zeroNesPos--] = 0;
 				while (zeroNesPos >= 0 && nums[zeroNesPos] == 0) zeroNesPos--;
-			}
+			// }
 
 			zero = zeroNesPos;
 			while (zero >= 0 && nums[zero] != 0) zero--;
