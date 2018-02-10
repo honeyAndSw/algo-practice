e152df7260c6915a056199d7e70709c3d81f0a43
honey.and.sw <honey.and.sw@gmail.com>
2018-02-10 19:33:40 +0900

    leetcode 523. Continuous Subarray Sum

--- src/main/java/leetcode/InorderSuccessorInBST285.java
+++ src/main/java/leetcode/InorderSuccessorInBST285.java
@@ -1,13 +1,35 @@
 package leetcode;
 
 public class InorderSuccessorInBST285 {
+	public TreeNode inorderSuccessor2(TreeNode root, TreeNode p) {
+		if (root == null) return null;
+		else if (root.val <= p.val) return inorderSuccessor2(root.right, p);
+		else {
+			TreeNode left = inorderSuccessor2(root.left, p);
+			return left == null ? root : left;
+		}
+	}
+
+	// Do we really need `parent`?
+	// `parent` is to remember parent node when going left subtree,
+	// because it's one of successor candidates.
+	// Then we just could left-side recursion first, then see current node is the answer or not.
+	//     5 <---- current
+	//   3   7
+	// 1  4
 	public TreeNode inorderSuccessor1(TreeNode root, TreeNode p) {
 		return inorderSuccessor1(root, p, null);
 	}
 
 	private TreeNode inorderSuccessor1(TreeNode root, TreeNode target, TreeNode parent) {
-		if (root.val == target.val) return target.right == null ? parent : leftMost(target.right);
+		// if (root.val == target.val) return target.right == null ? parent : leftMost(target.right);
+		// else if (root.val > target.val) return inorderSuccessor1(root.left, target, root);
+		// else return inorderSuccessor1(root.right, target, parent);
+
+		if (root == null) return parent;
 		else if (root.val > target.val) return inorderSuccessor1(root.left, target, root);
+		// Special care of `root.val == target.val` is not needed.
+		// Just do `leftMost` recursively.
 		else return inorderSuccessor1(root.right, target, parent);
 	}
 
