d58bdd969fbec01e9c56554af2513b57884f6a61
honey.and.sw <honey.and.sw@gmail.com>
2018-02-20 19:13:16 +0900

    leetcode 198. House Robber - starting from 0

---
+++ src/test/java/leetcode/HouseRobber198Test.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HouseRobber198Test {
+	@Test
+	public void test_mod_operator() throws Exception {
+		assertEquals(-2, -2 % 3);
+		assertEquals(1, (-2 + 3) % 3);
+	}
+}
