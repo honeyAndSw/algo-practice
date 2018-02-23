dee603a51a257b2ebb2daf1c64c30da2e630a7de
honey.and.sw <honey.and.sw@gmail.com>
2018-02-23 09:20:13 +0900

    leetcode 507. Perfect Number - remove duplicates - solutions: reduce ranges to check

--- src/main/java/leetcode/PerfectNumber507.java
+++ src/main/java/leetcode/PerfectNumber507.java
@@ -1,22 +1,32 @@
 package leetcode;
 
 public class PerfectNumber507 {
-	public boolean checkPerfectNumber(int num) {
+	public boolean checkPerfectNumber2(int num) {
 		int divisors = 0;
-		int d0 = 1, d1 = (num / 2), d = d0;
-
-		while (d <= d1) {
-			if (num % d == 0) {
-				if (d == 1) {
-					divisors += d;
-				} else {
-					divisors += d + (num / d);
-					d1 = (num / d) - 1;
+		for (int i = 1; i * i <= num; i++) {
+			if (num % i == 0) {
+				divisors += i;
+				if (i * i != num) {
+					divisors += num / i;
 				}
 			}
-			d++;
 		}
 
-		return num > 0 && num == divisors;
+		return divisors - num == num;
+	}
+
+	public boolean checkPerfectNumber(int num) {
+		int divisors = 1;
+		int start = 2, end = num / 2;
+		while (start <= end) {
+			if (num % start == 0) {
+				divisors += start + (num / start);
+				end = (num / start) - 1;
+			}
+
+			start++;
+		}
+
+		return num > 1 && num == divisors;
 	}
 }
