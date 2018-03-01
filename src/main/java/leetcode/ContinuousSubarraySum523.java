5d2d6076f43faffc59ee8f7a85ccc52a4c38599c
honey.and.sw <honey.and.sw@gmail.com>
2018-03-01 18:22:21 +0900

    leetcode 523. Continuous Subarray Sum - solution

--- src/main/java/leetcode/ContinuousSubarraySum523.java
+++ src/main/java/leetcode/ContinuousSubarraySum523.java
@@ -1,6 +1,37 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContinuousSubarraySum523 {
+
+	/*
+	 * Solution from discuss:
+	 * https://leetcode.com/problems/continuous-subarray-sum/discuss/99499/Java-O(n)-time-O(k)-space
+	 */
+	public boolean checkSubarraySum_solution(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>(){
+			{put(0,-1);}
+		};
+		int runningSum = 0;
+
+		for (int i=0;i<nums.length;i++) {
+			runningSum += nums[i];
+			if (k != 0) runningSum %= k;
+
+			Integer prev = map.get(runningSum);
+			// index        i i+1 ... j
+			// runningSum%k A  B  ... A
+			// At j prev is i, which means that
+			// (nums[i+i] + nums[i+2] + ... + nums[j-1]) % k is equal to 0,
+			// that (i, j] is the subarray sum to find.
+			if (prev != null && i - prev > 1) return true;
+			else map.put(runningSum, i);
+		}
+
+		return false;
+	}
+
 	public boolean checkSubarraySum(int[] nums, int k) {
 		k = Math.abs(k);
 
