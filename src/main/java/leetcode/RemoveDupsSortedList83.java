d84c79a4073a38b7fdbcd34d1731ce489f4d5719
honey.and.sw <honey.and.sw@gmail.com>
2018-03-25 09:50:59 +0900

    leetcode 83. Remove Duplicates from Sorted List

---
+++ src/main/java/leetcode/RemoveDupsSortedList83.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class RemoveDupsSortedList83 {
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode p = head;
+		while (p != null && p.next != null) {
+			if (p.val == p.next.val) p.next = p.next.next;
+			else p = p.next;
+		}
+		return head;
+	}
+}
