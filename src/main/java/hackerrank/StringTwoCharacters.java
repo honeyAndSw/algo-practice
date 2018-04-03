4edf106589254ace87aa4567c4394c4c872a807a
honey.and.sw <honey.and.sw@gmail.com>
2018-04-03 10:42:27 +0900

    hackerrank String/Two Characters - wrong answer

---
+++ src/main/java/hackerrank/StringTwoCharacters.java
@@ -0,0 +1,65 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringTwoCharacters {
+	public int twoCharaters(String s) {
+		// Initialization
+		int LEN = 26;
+		int[][] index = new int[LEN][LEN];
+		int[][] count = new int[LEN][LEN];
+
+		for (int i = 0; i < LEN; i++) {
+			int[] rowI = new int[LEN];
+			Arrays.fill(rowI, -1);
+			index[i] = rowI;
+
+			int[] rowC = new int[LEN];
+			rowC[i] = -1; // ignore count[i][i]
+			count[i] = rowC;
+		}
+
+		Set<Integer> unique = new HashSet<>();
+		for (int si = 0; si < s.length(); si++) {
+			char ch = s.charAt(si);
+			int ci = ch - 'a';
+			unique.add(ci);
+
+			for (int i = 0; i < LEN; i++) {
+				// Look at index[ch][?], (ch, ?)
+				if (count[ci][i] >= 0 && (index[ci][i] == -1 || s.charAt(index[ci][i]) != ch)) {
+					index[ci][i] = si;
+					count[ci][i] += 1;
+				} else {
+					count[ci][i] = -1;
+					count[i][ci] = -1;
+				}
+
+				// Look at index[?][ch], (?, ch)
+				// index[i][ci] should be initialized before.
+				if (index[i][ci] > -1) {
+					if (count[i][ci] >= 0 && s.charAt(index[i][ci]) != ch) {
+						index[i][ci] = si;
+						count[i][ci] += 1;
+					} else {
+						count[i][ci] = -1;
+						count[ci][i] = -1;
+					}
+				}
+			}
+		}
+
+		// find max
+		int max = 0;
+		for (int i = 0; i < LEN; i++) {
+			for (int j = 0; j < LEN; j++) {
+				if (unique.contains(i) && unique.contains(j) && count[i][j] > max) {
+					max = count[i][j];
+				}
+			}
+		}
+		return max;
+	}
+}
---
+++ src/test/java/hackerrank/StringTwoCharactersTest.java
@@ -0,0 +1,47 @@
+package hackerrank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringTwoCharactersTest {
+	StringTwoCharacters solution = new StringTwoCharacters();
+
+	@Test
+	public void test0() throws Exception {
+		assertEquals(4, solution.twoCharaters("abaacdabd"));
+		assertEquals(5, solution.twoCharaters("beabeefeab"));
+	}
+
+	@Test
+	public void test1() throws Exception {
+		assertEquals(0, solution.twoCharaters("aaa"));
+		assertEquals(0, solution.twoCharaters("abaa"));
+	}
+
+	@Test
+	public void test_testcase19() throws Exception {
+		String input =
+			"czoczkotespkfjnkbgpfnmtgqhorrzdppcebyybhlcsplqcqogqaszjgorlsrppinhgpaweydclepyftywafupqsjrbkqakpygolyyfksvqetrfzrcmatlicxtcxulwgvlnslazpfpoqrgssfcrfvwbtxaagjfahcgxbjlltfpprpcjyivxu";
+		assertEquals(6, solution.twoCharaters(input));
+		// (i,j)
+		// (3,13) 4
+		// (8,7) 5
+		// (8,20) 6
+		// (23,2) 9 --> (x,c)
+		// cccccccc xcxcxcxcx (9)
+		String test = input;
+		for (char c = 'a'; c <= 'z'; c++) {
+			if (c == 'x' || c == 'c') continue;
+			test = test.replace(c + "", "");
+		}
+		System.out.println(test);
+	}
+
+	@Test
+	public void test_testcase28() throws Exception {
+		String input =
+			"tlymrvjcylhqifsqtyyzfaugtibkkghfhyzxqbsizkjguqlqwwetyofqihtpkmpdlgthfybfhhmjerjdkybwppwrdapirukcshkpngayrdruanjtziknnwxmsjpnuswllymhkmztsrcwwzmlbcoakswwffveobbvzinkhnmvwqhpfednhsuzmffaebi";
+		assertEquals(0, solution.twoCharaters(input));
+	}
+}
