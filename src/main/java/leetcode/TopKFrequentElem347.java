96f0fcc2f77d2de76aa8e53d274fec9962153998
honey.and.sw <honey.and.sw@gmail.com>
2018-04-04 14:28:00 +0900

    WIP : heap time complexity

--- src/main/java/leetcode/TopKFrequentElem347.java
+++ src/main/java/leetcode/TopKFrequentElem347.java
@@ -4,13 +4,19 @@ import java.util.*;
 
 public class TopKFrequentElem347 {
     public List<Integer> topKFrequent(int[] nums, int k) {
+        // O(n), where n = nums.length
         Map<Integer, Integer> count = new HashMap<>();
         for (int n : nums) {
             int val = count.containsKey(n) ? count.get(n) : 0;
             count.put(n, val + 1);
         }
 
-        // reversed
+        /*
+         * Implementation note: this implementation provides
+         * O(log(n)) time for the enqueuing and dequeuing methods (offer, poll, remove and add);
+         * linear time for the remove(Object) and contains(Object) methods;
+         * and constant time for the retrieval methods (peek, element, and size).
+         */
         PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> count.get(o2) - count.get(o1));
 
         for (Integer key : count.keySet()) {
